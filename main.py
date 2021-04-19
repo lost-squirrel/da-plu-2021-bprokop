39e46bd2840d6349b5f25197df9e3d424af7fecd
Bartosz <bartosz.prokop@gmail.com>
2021-04-19 21:45:26 +0200

    Ex. 4

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, Request, HTTPException, Query, Response
 from typing import Optional
-import hashlib
+from hashlib import sha512
 from pydantic import BaseModel
+from datetime import date, timedelta
 
 app = FastAPI()
 app.counter = 0
+app.patient_id = 0
+app.mock_db = {}
 
 
 class HelloResp(BaseModel):
     msg: str
+
+
+class PatientData(BaseModel):
+    pid: int
+    name: str
+    surname: str
+    register_date: date
+    vaccination_date: date
 
 
 @app.get("/")
@@ -52,10 +63,26 @@ def method_post(request: Request):
     return {"method": request.method}
 
 
-@app.get("/auth")
+@ app.get("/auth")
 def auth_view(password: Optional[str] = Query(None), password_hash: Optional[str] = Query(None)):
     if password and password_hash and password.strip() and password_hash.strip():
-        calculated_hash = hashlib.sha512(password.encode()).hexdigest()
+        calculated_hash = sha512(password.encode()).hexdigest()
         if calculated_hash == password_hash:
             return Response(status_code=204)
     return Response(status_code=401)
+
+
+@ app.post("/register")
+def patient_register(name: str, surname: str):
+    app.patient_id += 1
+    register_date = date.today()
+    vaccination_date = register_date + timedelta(days=len(name) + len(surname))
+    patient = PatientData(
+        pid=app.patient_id,
+        name=name,
+        surname=surname,
+        register_date=register_date,
+        vaccination_date=vaccination_date
+    )
+    app.mock_db[app.patient_id] = patient
+    return patient
