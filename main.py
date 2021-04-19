871f190370c8c162e468267fdc4e26ac82723ebb
Bartosz <bartosz.prokop@gmail.com>
2021-04-19 19:59:39 +0200

    Ex 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from pydantic import BaseModel
 
@@ -24,3 +24,28 @@ def hello_name_view(name: str):
 def counter():
     app.counter += 1
     return app.counter
+
+
+@app.get("/method")
+def method_view(request: Request):
+    return {"method": request.method}
+
+
+@app.put("/method")
+def method_view(request: Request):
+    return {"method": request.method}
+
+
+@app.options("/method")
+def method_view(request: Request):
+    return {"method": request.method}
+
+
+@app.delete("/method")
+def method_view(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/method", status_code=201)
+def method_view(request: Request):
+    return {"method": request.method}
