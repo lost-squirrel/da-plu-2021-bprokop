6c04df69bae6c693687e568f034befccbdc4e64a
Bartosz <bartosz.prokop@gmail.com>
2021-04-19 20:55:49 +0200

    Ex 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
-
+from fastapi import FastAPI, Request, HTTPException, Query, Response
+from typing import Optional
+import hashlib
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -32,20 +33,29 @@ def method_view(request: Request):
 
 
 @app.put("/method")
-def method_view(request: Request):
+def method_put(request: Request):
     return {"method": request.method}
 
 
 @app.options("/method")
-def method_view(request: Request):
+def method_options(request: Request):
     return {"method": request.method}
 
 
 @app.delete("/method")
-def method_view(request: Request):
+def method_delete(request: Request):
     return {"method": request.method}
 
 
 @app.post("/method", status_code=201)
-def method_view(request: Request):
+def method_post(request: Request):
     return {"method": request.method}
+
+
+@app.get("/auth")
+def auth_view(password: Optional[str] = Query(None), password_hash: Optional[str] = Query(None)):
+    if password and password_hash and password.strip() and password_hash.strip():
+        calculated_hash = hashlib.sha512(password.encode()).hexdigest()
+        if calculated_hash == password_hash:
+            return Response(status_code=204)
+    return Response(status_code=401)
