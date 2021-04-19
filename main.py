8f6e01ed0e19498c8631e5d35af1ed0f8a564d28
Bartosz <bartosz.prokop@gmail.com>
2021-04-19 18:39:21 +0200

    Initial homework commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+app.counter = 0
+
+class HelloResp(BaseModel):
+    msg: str
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+@app.get("/counter")
+def counter():
+    app.counter += 1
+    return app.counter
