4dfa058cd82c7e084638847b8c68a9e649e8f41e
Bartosz <bartosz.prokop@gmail.com>
2021-05-11 17:30:43 +0200

    Praca Domowa 4 - Zadanie 6

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
 from typing import Optional
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 from starlette.requests import HTTPConnection
 from app.routers import deploy, art, fast
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -13,12 +14,18 @@ app.include_router(art.router)
 app.include_router(fast.router)
 
 
+class Category(BaseModel):
+    name: str
+
+
 @app.on_event('startup')
 async def startup():
     directory = os.path.dirname(__file__)
     filename = os.path.join(directory, "db/northwind.db")
     app.db_connection = sqlite3.connect(filename)
     app.db_connection.text_factory = lambda b: b.decode(errors="ignore")
+    app.cursor = app.db_connection.cursor()
+    app.cursor.row_factory = sqlite3.Row
 
 
 @app.on_event("shutdown")
@@ -28,20 +35,62 @@ async def shutdown():
 
 @app.get("/categories")
 async def categories_view():
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    categories = cursor.execute("""
+    categories = app.cursor.execute("""
     SELECT CategoryID as id, CategoryName as name
     FROM Categories
     ORDER BY UPPER(CategoryName)""").fetchall()
     return {"categories": categories}
 
 
+@app.post("/categories", status_code=201)
+async def add_category(category: Category):
+    cursor = app.cursor.execute("""
+    INSERT INTO Categories (CategoryName) 
+    VALUES (?)
+    """, (category.name,))
+    app.db_connection.commit()
+    new_category_id = cursor.lastrowid
+    category = app.cursor.execute("""
+    SELECT CategoryID as id, CategoryName as name
+    FROM Categories WHERE CategoryID = ?
+    """, (new_category_id,)).fetchone()
+    return category
+
+
+@app.put("/categories/{id}", status_code=200)
+async def update_category(id: int, category: Category):
+    app.cursor.execute("""
+    UPDATE Categories
+    SET CategoryName = ?
+    WHERE CategoryID = ?
+    """, (category.name, id))
+    app.db_connection.commit()
+    category = app.cursor.execute("""
+    SELECT CategoryID AS id, CategoryName AS name
+    FROM Categories
+    WHERE CategoryID = ?
+    """, (id, )).fetchone()
+    if not category:
+        raise HTTPException(404)
+    return category
+
+
+@app.delete("/categories/{id}", status_code=200)
+async def delete_category(id: int):
+    cursor = app.cursor.execute("""
+    DELETE FROM Categories
+    WHERE CategoryID = ?
+    """, (id, ))
+    app.db_connection.commit()
+    if not cursor.rowcount:
+        raise HTTPException(404)
+    return {"deleted": 1}
+
+
+@app.delete("/categories/{id}")
 @app.get("/customers")
 async def customers_view():
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    customers = cursor.execute("""
+    customers = app.cursor.execute("""
     SELECT CustomerID as id, COALESCE(CompanyName, '') as name,
     COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || COALESCE(Country, '') as full_address
     FROM Customers
@@ -52,12 +101,11 @@ async def customers_view():
 
 @app.get("/products/{id}")
 async def product_view(id: int):
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    product = cursor.execute("""
+    product = app.cursor.execute("""
     SELECT ProductID as id, ProductName as name
     FROM Products
     WHERE id=:id
+    ORDER BY UPPER(ProductID)
     """, {"id": id}).fetchone()
     if not product:
         raise HTTPException(404)
@@ -69,9 +117,7 @@ async def employees_view(order: Optional[str] = None, limit: int = -1, offset: i
     if order not in {'first_name', 'last_name', 'city', None}:
         raise HTTPException(400)
     order = order or 'id'
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    employees = cursor.execute("""
+    employees = app.cursor.execute("""
     SELECT EmployeeID as id, LastName as last_name, FirstName as first_name, City as city
     FROM Employees
     ORDER BY {} LIMIT {} OFFSET {}
@@ -81,9 +127,7 @@ async def employees_view(order: Optional[str] = None, limit: int = -1, offset: i
 
 @app.get("/products_extended")
 async def products_extended_view():
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    products_extended = cursor.execute("""
+    products_extended = app.cursor.execute("""
     SELECT Products.ProductID as id, Products.ProductName as name, Categories.CategoryName as category, Suppliers.CompanyName as supplier
     FROM Products
     JOIN Categories ON Products.CategoryID = Categories.CategoryID
@@ -94,9 +138,7 @@ async def products_extended_view():
 
 @app.get("/products/{id}/orders")
 async def products_orders_view(id: int):
-    cursor = app.db_connection.cursor()
-    cursor.row_factory = sqlite3.Row
-    orders = cursor.execute("""
+    orders = app.cursor.execute("""
     SELECT Orders.OrderID as id, Customers.CompanyName as customer, od.Quantity as quantity, round( od.Quantity * od.UnitPrice * (1-od.Discount) , 2) as total_price
     FROM Orders
     JOIN Customers ON Orders.CustomerID = Customers.CustomerID
